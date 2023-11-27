c745e7d4d9a0eaf48b1cc08bad7aaceaa47ea363
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-27 13:26:48 -0300

    Update com.js

--- assets/js/com.js
+++ assets/js/com.js
@@ -90,6 +90,18 @@ function postReply(id) {
             if ($(this).attr('data-date-minus')) {
                 $(this).html(dateMinus($(this).attr('data-date-minus')))
             }
+
+            // Defina a função setCookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
         })
         $('#' + id).after(replyc)
         obj.remove()
